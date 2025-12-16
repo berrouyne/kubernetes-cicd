6e57d9fa5feeb10bd5f5516e66a13a61655655a3
berrouyne <105479227+berrouyne@users.noreply.github.com>
2025-12-16 20:23:11 +0100

    Update index.js

--- app/index.js
+++ app/index.js
@@ -2,7 +2,7 @@ const express = require("express");
 const app = express();
 
 app.get("/", (req, res) => {
-  res.send("👋 Hello World from CI (GitHub Actions)!");
+  res.send("👋 Sensei was here!");
 });
 
 app.get("/healthz", (req, res) => {
